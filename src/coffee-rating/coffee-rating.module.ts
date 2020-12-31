b307a820fee9a594d9a0d041d2d19302fb8942b5
owen_adira <owenadira@gmail.com>
2020-12-31 10:26:35 +0300

    import missing module

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
-import { CoffeesService } from '../coffees/coffees.service';
+import { CoffeesModule } from '../coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesService],
+  imports: [CoffeesModule],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
